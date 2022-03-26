83f71fc2ebf988a601f505e6bb8e4681ca3e9ef5
Maslik <maslenkov2010@gmail.com>
2022-03-26 16:51:04 +0300

    JS

--- HW_8.js
+++ HW_8.js
@@ -5,10 +5,28 @@ class Marker {
     constructor(color,inkQuantity) {
         this.color = color;
         this.inkQuantity = inkQuantity;
+
     }
-    
+    function(){
+        return console.log(this.color +" " + this.inkQuantity + " %")
+    }
 
 }
+class Zapravka{
+    constructor(color,inkQuantity) {
+        this.color = marker.color;
+        this.inkQuantity = marker.inkQuantity;
+    }
+    function(){
+    for (let a = 0; a<3; a++){
+        this.color = marker.color;
+        this.inkQuantity += 25;
 
-let marker= new Marker('red',100);
-console.log(marker);
+    }
+        return console.log(this.color +" " + this.inkQuantity + " %")
+        }
+}
+let marker= new Marker('red',25);
+let zapravka = new Zapravka(Marker.prototype.color,Marker.prototype.inkQuantity)
+marker.function();
+zapravka.function();
