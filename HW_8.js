bd0fff71f06504b976671ebdcb8155f2daf0da28
Maslik <maslenkov2010@gmail.com>
2022-03-26 16:34:24 +0300

    JS

---
+++ HW_8.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class Marker {
+
+    constructor(color,inkQuantity) {
+        this.color = color;
+        this.inkQuantity = inkQuantity;
+    }
+    
+
+}
+
+let marker= new Marker('red',100);
+console.log(marker);
