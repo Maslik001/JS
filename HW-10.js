f953d7c71d5fabd775a26140197384f8f6953024
Maslik <maslenkov2010@gmail.com>
2022-04-05 17:36:28 +0300

    JS

--- HW-10.js
+++ HW-10.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const x = 34;
+const x = 22;
 const y = 28;
 let d = [];
 
@@ -8,9 +8,6 @@ function integer(x, y) {
 
     if (y - 1 === x) {
         return x - 1;
-    }
-    if (x > y) {
-      return   console.log('неверный диапазон')
     }
     d.push(x + 1)
     return x++ + integer(x, y);
