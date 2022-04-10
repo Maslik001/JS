13896f101f9da9ef95732ac36043c356adaf30d0
Maslik <maslenkov2010@gmail.com>
2022-04-10 13:59:26 +0300

    JS

--- HW-10.js
+++ HW-10.js
@@ -20,36 +20,99 @@ console.log(d.sort())
 
 
 ////****** Task 2  ***** /////
-/*
 
-let fibMas= [];
-function fibonachi(n){
-    if (n===0) {
-        fibMas.push(n)
-
-        return fibonachi(n+1)
-
+/*function fibonachi(n){
+    let a =0;
+    let b= 1;
+    console.log(a);
+    console.log(b);
+    for (let i = 0 ; i <= n; i++){
+        let c = a+b;
+        a=b;
+        b=c;
+        console.log(c)
     }
-
-    fibMas.push(fibMas[fibMas.length-1]+n)
-
-    return fibonachi(fibMas.length-1);
-
 }
-fibonachi(0)
-console.log(fibMas)*/
+fibonachi(5)*/
+
+/*let a = 0;
+let b = 1;
+console.log(a);
+console.log(b);
+function fibonachi(n) {
+    --n;
+    if (n === 0) return ;
+    let c = a+b;
+    a=b;
+    b=c;
+    console.log(c);
+    fibonachi(n);
+}
+fibonachi(7)*/
 
 
 ////****** Task 3  ***** /////
-/*
-const numbers = [1, 2, 3, 1, 2];
+// const numbers = [1, 2, 3, 1, 2,4,4];
 
-function double(num) {
-    num.forEach(function(elem){
-        console.log(elem)
-    })
-
-
-    // return double(num)
+/*function double(num) {
+    for (let i = 0; i <= num.length - 1; i++) {
+        for (let j = 1 + i; j <= num.length; j++) {
+            if (num[i] === num[j]) {
+                num.splice(j, num[i]);
+            }
+        }
+    }
+    console.log(numbers);
 }
+double(numbers);*/
+
+/*let counter = 0;
+function double(num) {
+    let a = num[num[counter]]
+    let b = num[num.length-counter]
+    if(a===b){
+        num.splice(1,b);
+        console.log(num)
+    }
+    counter++;
+    if (counter>num.length) return;
+    double(num)
+
+}
+
 double(numbers)*/
+
+////****** Task 4  ***** /////
+/*function sum(number){
+    if (number<10){
+        console.log(number);
+        return;
+    }
+    let a = number%10;
+    let b = (number-a)/10;
+    let res= a+b;
+    if (res<= 9) {
+        console.log(res);
+        return;
+    }
+    number = res;
+    sum(number);
+}
+sum(151654);*/
+
+////****** Task 5  ***** /////
+
+let text = 'var_text_hello';
+
+function funcText() {
+    let arr = text.split('_')
+    for (let i = 0; i < arr.length; i++){
+        let b = arr[0].toUpperCase() + arr.slice(1);
+        console.log(b)
+    }
+
+    console.log(arr)
+}
+
+funcText()
+
