d1c1da31c8e10c3522e29aa24231fb473bc9771f
Maslik <maslenkov2010@gmail.com>
2022-04-17 15:32:42 +0300

    JS

--- HW-15.js
+++ HW-15.js
@@ -1,2 +1,30 @@
 'use strict';
 
+const poll = {
+    question: "Какой ваш любимый язык программирования?",
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    answers: new Array(4).fill(0),
+};
+console.log(poll.answers)
+
+function registerNewAnswer(a) {
+    if (a < 4) {
+        let counter = this.answers[a];
+        counter++;
+        for (let i in this.answers) {
+            a == i ? this.answers.splice(a, 1, counter) : false;
+        }
+    } else {
+        console.log('Попробуйте еще раз');
+        // return input();
+    }
+}
+
+function input() {
+    let a = 3;
+    registerNewAnswer.call(poll, a);
+}
+
+input()
+console.log(poll.answers);
+// poll.call(poll,registerNewAnswer);
