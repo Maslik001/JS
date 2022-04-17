2defce0de0e7ab38e3a18c98cfaf6355d792e55a
Maslik <maslenkov2010@gmail.com>
2022-04-17 15:32:42 +0300

    JS

--- HW-15.js
+++ HW-15.js
@@ -1,2 +1,21 @@
 'use strict';
 
+let resultArr = [0, 0, 0, 0];
+const resultArea = document.getElementById('result');
+const opros = document.getElementById('opros');
+const golosBtn = document.getElementById('golos');
+golosBtn.addEventListener('click', () => {
+    const allRadio = opros.querySelectorAll('.radio');
+    resultArea.textContent = '';
+    console.log(allRadio);
+    allRadio.forEach((item, index) => {
+        if (item.checked) {
+            // resultArea.append(index);
+            resultArr[index]++;
+            console.dir(resultArr);
+            // opros.style.marginTop = '10px';
+            // opros.style.border='none';
+            // opros.style.backgroundColor='lightgreen';
+    }
+})
+})
