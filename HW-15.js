cb464bdc61921be0e00b5c2cc76e9327551b7b71
Maslik <maslenkov2010@gmail.com>
2022-04-19 17:40:19 +0300

    JS

--- HW-15.js
+++ HW-15.js
@@ -4,18 +4,32 @@ let resultArr = [0, 0, 0, 0];
 const resultArea = document.getElementById('result');
 const opros = document.getElementById('opros');
 const golosBtn = document.getElementById('golos');
+const newOpros = document.getElementById('newOpros');
+const resOpros = document.getElementById('resOpros')
+
+
+newOpros.addEventListener('click',()=>{
+    opros.style.display = 'flex';
+    newOpros.style.display = 'none';
+
+})
 golosBtn.addEventListener('click', () => {
     const allRadio = opros.querySelectorAll('.radio');
     resultArea.textContent = '';
     console.log(allRadio);
     allRadio.forEach((item, index) => {
         if (item.checked) {
-            // resultArea.append(index);
+            resultArea.append(`Ваш ответ: ${index}: ${allRadio[index].nextElementSibling.innerText}`);
             resultArr[index]++;
-            console.dir(resultArr);
-            // opros.style.marginTop = '10px';
-            // opros.style.border='none';
-            // opros.style.backgroundColor='lightgreen';
+            opros.style.display = 'none';
+            let text = 'Спасибо что прошли опрос';
+            opros.insertAdjacentText('afterend', text);
+            resOpros.style.display = 'block';
     }
+
 })
+})
+resOpros.addEventListener('click',()=>{
+
+
 })
