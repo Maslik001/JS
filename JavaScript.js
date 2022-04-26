8e5c471fe22f197120cdc929bf841c41f99eb158
Maslik <maslenkov2010@gmail.com>
2022-04-26 17:31:20 +0300

    fix Task

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const closeCalc = document.getElementById('close');
+let closeCalc;
 const calnOn = document.getElementById('calc-img');
-const calcWrapper = document.getElementById('calc-wrapper');
+let calcWrapper;
 const trash = document.getElementById('trash');
 const trashCleaner = document.getElementById('trashiconCleaner');
 const cleaner = document.getElementById('cleaner');
@@ -10,136 +10,15 @@ const folder = document.getElementById('cleanerfolder');
 const delFolder = document.getElementById('delFolder');
 const body = document.getElementById('body');
 const effectPromt = document.getElementById('effect-prompt');
-const calc = document.getElementById('calc');
-const resBlock = document.getElementById('resBlock');
-const memoryBtn = document.getElementById('memory')
+let calc;
+let resBlock;
+const memoryBtn = document.getElementById('memory');
+const memSymbol = document.getElementById('mem');
 let isOperation;
 let result = '';
 let num2 = '';
 let memory = '';
 
-/**
- * Функция для подсчёта результатов операции
- * @param param1 - ввреденное число
- * @param simbol - оператор
- */
-function calcOper(number, operation) {
-    num2 = '';
-    result = result * 1;
-    number = number * 1;
-    switch (operation) {
-        case '+':
-            return result =  result + number;
-        case '-':
-            return result = result - number;
-        case '/':
-            return result = result / number;
-        case '*':
-            return result = result * number;
-    }
-
-}
-
-function memoryF(){
-    if (memory === '') {
-        memory = result*1;
-      return   resBlock.textContent = `${memory}`;
-
-    } else if(memory !== '' && isOperation){
-        num2 = memory
-        calcOper(num2, isOperation);
-        return  resBlock.textContent = `${memory}`;
-    }
-    else {
-        result = Number(result);
-         result = memory
-        calcOper(result, isOperation)
-       return  resBlock.textContent = `${memory}`;
-    }
-
-}
-
-
-// console.log(number.lastIndexOf('0'),number.length);
-
-calc.addEventListener('click', (e) => {
-    let elem = e.target;
-    while (!elem.classList.contains('calc-btn')) {
-        elem = elem.parentNode;
-    }
-    const dataNum = elem.dataset.symbol;
-    resBlock.insertAdjacentText('beforeend', `${dataNum}`);
-    metka:
-    if (Number(dataNum) || dataNum === 'M+') {
-        // resBlock.textContent = "";
-
-
-        if (isOperation || dataNum === 'M+') {
-            if (dataNum === 'M+') {
-                memoryF();
-                memoryBtn.style.backgroundColor = 'aqua';
-                // num2 = memory;
-            } else {
-                num2 += dataNum;
-            }
-        } else {
-            result += dataNum;
-        }
-    } else if (dataNum === '=') {
-        calcOper(num2, isOperation);
-        resBlock.textContent = "";
-        resBlock.insertAdjacentText('beforeend', `${result}`);
-        isOperation = '';
-        result = '';
-        break metka;
-
-    } else if (dataNum === 'del') {
-        result = '';
-        num2 = '';
-        isOperation = '';
-        resBlock.textContent = "";
-
-    }
-    // else if (dataNum === 'M+') {
-    //     resBlock.textContent = `${memory}`;
-    //     if (memory === "") {
-    //         memory = result;
-    //         resBlock.textContent = `${memory}`;
-    //     } else {
-    //         result = memory
-    //          calcOper(result, isOperation);
-    //         resBlock.textContent = `${memory}`;
-    //     }
-    //     memoryBtn.style.backgroundColor = 'aqua'
-    // }
-    else if (dataNum === 'M-') {
-        memory = '';
-        resBlock.textContent = "";
-        memoryBtn.style.backgroundColor = 'white';
-    } else {
-        isOperation = dataNum;
-    }
-    console.log(elem.dataset.symbol);
-
-});
-
-
-calnOn.addEventListener('click', () => {
-    calcWrapper.classList.add('animate__fadeInBottomLeft')
-    calcWrapper.style.display = 'flex';
-    setTimeout(function () {
-        calcWrapper.classList.remove('animate__fadeInBottomLeft')
-    }, 1200)
-
-})
-
-closeCalc.addEventListener('click', () => {
-    calcWrapper.classList.add('animate__fadeOutBottomLeft')
-    setTimeout(function () {
-        calcWrapper.style.display = 'none';
-        calcWrapper.classList.remove('animate__fadeOutBottomLeft')
-    }, 1000)
-})
 
 trash.addEventListener('contextmenu', (e) => {
     e.preventDefault();
@@ -198,3 +77,161 @@ function keyCodeF(e) {
         effectPromt.style.display = 'none';
     }
 }
+
+
+/**
+ * Функция для подсчёта результатов операции
+ * @param param1 - ввреденное число
+ * @param simbol - оператор
+ */
+function calcOper(number, operation) {
+    num2 = '';
+    result = result * 1;
+    number = number * 1;
+    switch (operation) {
+        case '+':
+            return result = result + number;
+        case '-':
+            return result = result - number;
+        case '/':
+            if (result === 0 || number===0){
+                return resBlock.textContent = `Ошибка`;
+            } else {
+                return result = result / number;
+            }
+        case '*':
+            return result = result * number;
+    }
+
+}
+
+/**
+ * Работа с памятью
+ * @returns {string}
+ */
+function memoryF() {
+    memSymbol.style.display = 'flex';
+    if (memory === '') {
+        memory = result * 1;
+        return resBlock.textContent = `${memory}`;
+
+    } else if (memory !== '' && isOperation) {
+        num2 = memory
+        calcOper(num2, isOperation);
+        return resBlock.textContent = `${memory}`;
+    } else {
+        result = Number(result);
+        result = memory
+        calcOper(result, isOperation)
+        return resBlock.textContent = `${memory}`;
+    }
+
+}
+
+
+/**
+ * добавление калькулятора на страницу
+ */
+calnOn.addEventListener('click', () => {
+        calcWrapper = document.getElementById('calc-wrapper');
+        let newCalc = `<div class="res" id="resBlock"></div>
+        <div class="close" id="close">close</div>
+        <div class="mem" id="mem">M</div>
+        <div class="calc" id="calc">
+            <div class="calc-btn" data-symbol="del">C</div>
+            <div class="calc-btn memory" id="memory" data-symbol="M+">M+</div>
+            <div class="calc-btn" data-symbol="M-">M-</div>
+            <div class="calc-btn" data-symbol="/">&divide;</div>
+            <div class="calc-btn" data-symbol="7">7</div>
+            <div class="calc-btn" data-symbol="8">8</div>
+            <div class="calc-btn" data-symbol="9">9</div>
+            <div class="calc-btn" data-symbol="*">&times;</div>
+            <div class="calc-btn" data-symbol="4">4</div>
+            <div class="calc-btn" data-symbol="5">5</div>
+            <div class="calc-btn" data-symbol="6">6</div>
+            <div class="calc-btn" data-symbol="-">-</div>
+            <div class="calc-btn" data-symbol="1">1</div>
+            <div class="calc-btn" data-symbol="2">2</div>
+            <div class="calc-btn" data-symbol="3">3</div>
+            <div class="calc-btn" data-symbol="+">+</div>
+            <div class="calc-btn grid" data-symbol="0">0</div>
+            <div class="calc-btn" data-symbol="=">=</div></div>`
+        calcWrapper.insertAdjacentHTML("afterbegin", newCalc);
+        calc = document.getElementById('calc');
+        resBlock = document.getElementById('resBlock');
+        closeCalc = document.getElementById('close');
+        calcWrapper.classList.add('animate__fadeInBottomLeft')
+        calcWrapper.style.display = 'flex';
+        setTimeout(function () {
+            calcWrapper.classList.remove('animate__fadeInBottomLeft')
+        }, 1200)
+        closeCalc.addEventListener('click', () => {
+
+            calcWrapper.classList.add('animate__fadeOutBottomLeft')
+            setTimeout(function () {
+                calcWrapper.style.display = 'none';
+                calcWrapper.classList.remove('animate__fadeOutBottomLeft')
+                while (calcWrapper.firstChild) {
+                    calcWrapper.removeChild(calcWrapper.firstChild);
+                }
+            }, 1000)
+
+        })
+        initialization();
+});
+
+
+function initialization() {
+    calc.addEventListener('click', (e) => {
+
+        let elem = e.target;
+        while (!elem.classList.contains('calc-btn')) {
+            elem = elem.parentNode;
+        }
+        const dataNum = elem.dataset.symbol;
+        resBlock.insertAdjacentText('beforeend', `${dataNum}`);
+        metka:
+            if (Number(dataNum) || dataNum === 'M+' || dataNum === '0') {
+                // resBlock.textContent = "";
+                if (isOperation || dataNum === 'M+') {
+                    // resBlock.textContent = "";
+                    if (dataNum === 'M+') {
+                        memoryF();
+                        // memoryBtn.style.backgroundColor = 'aqua';
+                    } else if (num2 !== '') {
+                        num2 += dataNum;
+                    } else {
+                        num2 += dataNum;
+                    }
+
+                } else {
+                    result += dataNum;
+                }
+            } else if (dataNum === '=') {
+                result = calcOper(num2, isOperation);
+                resBlock.textContent = "";
+                resBlock.insertAdjacentText('beforeend', `${result}`);
+                isOperation = '';
+                // result = '';
+                break metka;
+
+            } else if (dataNum === 'del') {
+                result = '';
+                num2 = '';
+                isOperation = '';
+                resBlock.textContent = "";
+
+            } else if (dataNum === 'M-') {
+                memory = '';
+                resBlock.textContent = "";
+                memSymbol.style.display = 'none';
+                // memoryBtn.style.backgroundColor = 'white';
+            } else {
+                isOperation = dataNum;
+            }
+        // console.log(elem.dataset.symbol);
+
+    });
+}
+
+
