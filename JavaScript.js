305c53d60b89d0b2e6abfe0f196e25571f37e82f
Maslik <Maslenkov2010@gmail.com>
2022-05-04 22:57:47 +0300

    fix Task

--- JavaScript.js
+++ JavaScript.js
@@ -2,8 +2,8 @@
 
 
 //*********** TASK 1 **************///
-/*
 
+/*
 let Car = function (make, speed) {
     this.make = make;
     this.speed  = speed ;
@@ -11,10 +11,10 @@ let Car = function (make, speed) {
 
 Car.prototype.accelerate = function () {
     console.log(`${this.speed+10}`)
-    return this.speed*1 + 10;
+    return this.speed + 10;
 }
 Car.prototype.brake = function () {
-    console.log(`${this.speed}`-5)
+    console.log(`${this.speed-5}`)
     return this.speed - 5;
 }
 let bmw = new Car('BMW', 120);
@@ -26,23 +26,40 @@ console.log(`${bmw.make} увеличивает скорость до ${bmw.acce
 */
 
 //*********** TASK 2 **************///
-
+/*
 class CarCl {
     constructor(make, speed) {
         this.make = make;
         this.speed = speed;
     }
-
     get speedUS(){
-        return this.speed /1.6;
+        return this.speed / 1.6;
     }
-    set speedUS(v){
-       this.speed = v * 1.6;
+    set speedUS(mill){
+       this.speed = mill * 1.6;
     }
-}
+    accelerate () {
+        this.speed += 10;
+        return this;
+    }
+    brake () {
+        return this.speed - 5;
+    }
+}*/
 
-let bmw = new CarCl('BMW', 120);
+/*let bmw = new CarCl('BMW', 180);
+console.log(bmw);
+
 console.log(`Скорость ${bmw.make}: ${bmw.speedUS} mph`);
 let mill = bmw.speedUS;
 bmw.speedUS = mill;
-console.log(bmw.speedUS);
+console.log(`Скорость ${bmw.make}: ${bmw.speed} kmh`);*/
+
+//---- 4 ---- //
+/*
+let ford = new CarCl('FORD', 120);
+console.log(`Скорость ${ford.make}: ${ford.speedUS} mph`);
+ford.speedUS = 75;
+console.log(`Скорость ${ford.make}: ${ford.speed} kmh`);
+console.log(`${ford.make} снижает скорость до ${ford.brake()} км/ч`);
+console.log(`${ford.make} увеличивает скорость до ${ford.accelerate().speedUS} mph`);*/
