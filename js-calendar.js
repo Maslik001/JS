434cab4beff9be540cf6714e26068f62143ba4fc
Maslik <maslenkov2010@gmail.com>
2022-05-25 17:24:41 +0300

    start calendar

--- js-calendar.js
+++ js-calendar.js
@@ -1,3 +1,14 @@
 'use strict';
+const option = {
+    // timeZone: `Etc/GMT${'-7'}`,
+    month: 'long',
+    year: 'numeric',
+    day: 'numeric',
+    weekday: 'long',
+    hour: '2-digit',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZoneName: 'short'
+}
 
-
+const now = new Date();
