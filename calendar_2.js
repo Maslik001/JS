c3fddf43f7fd884a4e55939c484cfc859497ad56
Maslik <Maslenkov2010@gmail.com>
2022-08-30 19:21:46 +0200

    Fix weather bug

--- calendar_2.js
+++ calendar_2.js
@@ -25,7 +25,6 @@ window.addEventListener('load', () => {
 
 
 calendarIco.addEventListener('click', () => {
-    console.log('actual =======')
     if (!calendarStatus) {
         calendar = document.querySelector('.calendar-wrapper');
         let addCalendar = `
