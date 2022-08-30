396075c95865034d3c750982fe88fd8fc7196811
Maslik <Maslenkov2010@gmail.com>
2022-08-30 19:24:09 +0200

    name Calc

--- calendar_2.js
+++ calendar_2.js
@@ -25,6 +25,7 @@ window.addEventListener('load', () => {
 
 
 calendarIco.addEventListener('click', () => {
+    console.log('Calc')
     if (!calendarStatus) {
         calendar = document.querySelector('.calendar-wrapper');
         let addCalendar = `
